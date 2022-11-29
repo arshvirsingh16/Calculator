6edb36784ad2143945f813c2bc148a6be1573c8e
Arshvir Singh <arshvirsignh16@gmail.com>
2022-11-29 01:40:07 -0800

    create operation functions and renamed ids

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // constants
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll(".button");
+const screen = document.querySelector("#screen");
 
 
 //variables
@@ -9,6 +10,45 @@ buttons.forEach(button => {
     button.addEventListener("click", (e) => {
         selection = button.value;
         console.log(button.value);
+        screen.innerHTML = selection;
     })
 })
+
+function operation(selection) {
+    switch (selection){
+        case '+' : add_func();
+        case '-' : sub_func();
+        case 'x' : mult_func();
+        case '/' : divi_func();
+        default  : return;
+    }
+}
+
+function add_func(num_one, num_two) {
+    let result = 0;
+    result = num_one + num_two;
+    return result;
+}
+
+function sub_func(num_one, num_two) {
+    let result = 0;
+    result = num_one - num_two;
+    return result;
+}
+
+function mult_func(num_one, num_two) {
+    let result = 0;
+    result = num_one * num_two;
+    return result;
+}
+
+function divi_func(num_one, num_two) {
+    let result = 0;
+    result = num_one / num_two;
+    return result;
+}
+
+
+
+
 
