dcc02947dc43946a514706bbe7e296d73aff6526
Arshvir Singh <arshvirsignh16@gmail.com>
2022-11-29 02:21:39 -0800

    test if save number works

--- main.js
+++ main.js
@@ -1,20 +1,51 @@
 // constants
 const buttons = document.querySelectorAll(".button");
 const screen = document.querySelector("#screen");
+const num_buttons = document.querySelectorAll("#num_button");
+const op_buttons = document.querySelectorAll("#op_button");
 
 
-//variables
+//global variables
 let selection;
+let operation;
+let picked_op;
+let temp_num_one = 0;
+let num_one;
+let num_two = 0;
 
 buttons.forEach(button => {
     button.addEventListener("click", (e) => {
         selection = button.value;
-        console.log(button.value);
+        let id = button.id;
+        // console.log(selection);
+        // console.log(num_one);
         screen.innerHTML = selection;
     })
 })
 
-function operation(selection) {
+num_buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        selection = button.value;
+        temp_num_one += selection;
+        if(picked_op == 'op_button'){
+            picked_op = null;
+            num_one = temp_num_one;
+            console.log(num_one); 
+        }
+        
+    })
+})
+
+op_buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        operation = button.value;
+        picked_op = button.id;
+    })
+})
+
+
+
+function calc_operation(selection) {
     switch (selection){
         case '+' : add_func();
         case '-' : sub_func();
