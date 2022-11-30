9ce30c449996603858fdefc0a3b7bcee44bb6787
Arshvir Singh <arshvirsignh16@gmail.com>
2022-11-30 01:36:07 -0800

    create a clear button, implemented to clear screen

--- main.js
+++ main.js
@@ -9,49 +9,55 @@ const op_buttons = document.querySelectorAll("#op_button");
 let selection;
 let operation;
 let picked_op;
-let temp_num = 0;
+let temp_num = '';
+let id;
 let num_one = null;
-let num_two = 0;
-let getNUM = 'one';
+let num_two = null;
 
 buttons.forEach(button => {
     button.addEventListener("click", (e) => {
         selection = button.value;
-        let id = button.id;
+        create_num(selection);
+         id = button.id;
+         console.log(selection);
         // console.log(selection);
         // console.log(num_one);
-        screen.innerHTML = selection;
+        screen.innerHTML = temp_num;
     })
 })
 
-num_buttons.forEach(button => {
-    button.addEventListener("click", (e) => {
-        selection = button.value;
-        temp_num += selection;
-        console.log(temp_num);
-        if(picked_op == 'op_button' && getNUM == 'one'){
-            picked_op = null;
-            num_one = temp_num;
-            getNUM = 'two'
-            console.log('is '+num_one); 
-        }
+// num_buttons.forEach(button => {
+//     button.addEventListener("click", (e) => {
+//         // selection = button.value;
+//         temp_num += selection;
+//         console.log(temp_num);
+//         // if(picked_op == 'op_button' && getNUM == 'one'){
+//         //     picked_op = null;
+//         //     num_one = temp_num;
+//         //     getNUM = 'two'
+//         //     console.log('is '+num_one); 
+//         // }
 
-        if(num_one == null && picked_op == 'op_button' && getNUM == 'two'){
-            picked_op = null;
-            num_two = temp_num;
-            getNUM = 'one';
-            console.log(num_two + 'istwo');
-        }
+//         // if(num_one == null && picked_op == 'op_button' && getNUM == 'two'){
+//         //     picked_op = null;
+//         //     num_two = temp_num;
+//         //     getNUM = 'one';
+//         //     console.log(num_two + 'istwo');
+//         // }
         
-    })
-})
+//     })
+// })
 
-op_buttons.forEach(button => {
-    button.addEventListener("click", (e) => {
-        operation = button.value;
-        picked_op = button.id;
-    })
-})
+// op_buttons.forEach(button => {
+//     button.addEventListener("click", (e) => {
+//         operation = button.value;
+//         picked_op = button.id;
+//     })
+// })
+
+
+
+
 
 
 
@@ -87,6 +93,21 @@ function divi_func(num_one, num_two) {
     let result = 0;
     result = num_one / num_two;
     return result;
+}
+
+function create_num(selection){
+    if(selection == 'clr' || selection =='='){
+        temp_num = '';
+        return null;
+    }
+    temp_num += selection;
+    screen.innerHTML = temp_num;
+}
+
+function clear_num(selection) {
+    if(selection == 'clr'){
+        screen.innerHTML = '';
+    }
 }
 
 
