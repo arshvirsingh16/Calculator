c72bd0fc2f8cf93f658582436c3edc9d77f9dcd4
Arshvir Singh <arshvirsignh16@gmail.com>
2022-11-30 01:56:56 -0800

    try to create a store function

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const buttons = document.querySelectorAll(".button");
 const screen = document.querySelector("#screen");
 const num_buttons = document.querySelectorAll("#num_button");
 const op_buttons = document.querySelectorAll("#op_button");
-
+const num_array = [];
 
 //global variables
 let selection;
@@ -13,16 +13,15 @@ let temp_num = '';
 let id;
 let num_one = null;
 let num_two = null;
+let i = 0;
 
 buttons.forEach(button => {
     button.addEventListener("click", (e) => {
         selection = button.value;
         create_num(selection);
+        clear_num(selection);
          id = button.id;
          console.log(selection);
-        // console.log(selection);
-        // console.log(num_one);
-        screen.innerHTML = temp_num;
     })
 })
 
@@ -48,67 +47,73 @@ buttons.forEach(button => {
 //     })
 // })
 
-// op_buttons.forEach(button => {
-//     button.addEventListener("click", (e) => {
-//         operation = button.value;
-//         picked_op = button.id;
-//     })
-// })
+op_buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        operation = button.value;
+        store_num(temp_num);
+        // console.log(operation);
+    })
+})
 
 
-
-
-
-
-
-function calc_operation(selection) {
-    switch (selection){
-        case '+' : add_func();
-        case '-' : sub_func();
-        case 'x' : mult_func();
-        case '/' : divi_func();
-        default  : return;
-    }
-}
-
-function add_func(num_one, num_two) {
-    let result = 0;
-    result = num_one + num_two;
-    return result;
-}
-
-function sub_func(num_one, num_two) {
-    let result = 0;
-    result = num_one - num_two;
-    return result;
-}
-
-function mult_func(num_one, num_two) {
-    let result = 0;
-    result = num_one * num_two;
-    return result;
-}
-
-function divi_func(num_one, num_two) {
-    let result = 0;
-    result = num_one / num_two;
-    return result;
-}
 
 function create_num(selection){
-    if(selection == 'clr' || selection =='='){
-        temp_num = '';
-        return null;
-    }
+    // if(selection == 'clr' || selection =='='){
+    //     temp_num = '';
+    //     screen.innerHTML = temp_num;
+    //     return null;
+    // }
     temp_num += selection;
     screen.innerHTML = temp_num;
 }
 
 function clear_num(selection) {
-    if(selection == 'clr'){
-        screen.innerHTML = '';
+    if(selection == 'clr' || selection =='='){
+        temp_num = '';
+        screen.innerHTML = temp_num;
+        return null;
     }
 }
+
+function store_num(temp_num){
+    num_array[i] = temp_num;
+    screen.innerHTML = num_array;
+    i++;
+}
+
+// function calc_operation(selection) {
+    //     switch (selection){
+    //         case '+' : add_func();
+    //         case '-' : sub_func();
+    //         case 'x' : mult_func();
+    //         case '/' : divi_func();
+    //         default  : return;
+    //     }
+    // }
+    
+    // function add_func(num_one, num_two) {
+    //     let result = 0;
+    //     result = num_one + num_two;
+    //     return result;
+    // }
+    
+    // function sub_func(num_one, num_two) {
+    //     let result = 0;
+    //     result = num_one - num_two;
+    //     return result;
+    // }
+    
+    // function mult_func(num_one, num_two) {
+    //     let result = 0;
+    //     result = num_one * num_two;
+    //     return result;
+    // }
+    
+    // function divi_func(num_one, num_two) {
+    //     let result = 0;
+    //     result = num_one / num_two;
+    //     return result;
+    // }
 
 
 
