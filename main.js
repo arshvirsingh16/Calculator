43e3bff837fd473fef308fc3890b38b493d659d8
Arshvir Singh <arshvirsignh16@gmail.com>
2022-12-28 22:56:44 -0800

    Allows calculator to do simple calculations w bugs

--- main.js
+++ main.js
@@ -3,33 +3,85 @@ const num_buttons = document.querySelectorAll('[data-num]');
 const op_buttons = document.querySelectorAll('[data-op]');
 const del_button = document.querySelector('[data-del]');
 const AC_button = document.querySelector('[data-AC]');
+const equal_button = document.querySelector('[data-equal]');
+const screen = document.querySelector('.screen');
+
+let previousNumber = '';
+let number = '';
+let operator = '';
+let computation = '';
+
 num_buttons.forEach(button => {
-    button.addEventListener('click', create_num());
+    button.addEventListener('click', (e) =>{
+        createNum(button.innerHTML);
+        updateDisplay();
+    });
 })
 
-class Calculator{
-    clearAll(){
+op_buttons.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        chooseOperator(button.innerHTML);
+    });
+})
 
-    }
+AC_button.addEventListener('click', allClear);
+del_button.addEventListener('click', deleteNumber);
+equal_button.addEventListener('click', calculate);
 
-    delete(){
+function createNum(e){
+    if(e === '.' && number.includes('.')) 
+        return;
+    number += e;
+    console.log(number);
+}
 
-    }
+function updateDisplay(){
+    screen.innerHTML = number;
+}
 
-    updateOperator(){
+function allClear(){
+    screen.innerHTML = '';
+    previousNumber = '';
+    number = '';
+}
 
-    }
-
-    createNum(){
-
-    }
-
-    calculate(){
-
-    }
+function deleteNumber(){
 
 }
 
+function chooseOperator(e){
+    if(previousNumber != '' && number != '')
+        calculate();
+    if(previousNumber != '' && e) return;
 
+    operator = e;
+    screen.innerHTML = number + e;
+    previousNumber = number;
+    number = '';
+}
 
-
+function calculate(){
+    const previousValue = parseFloat(previousNumber);
+    const currentValue = parseFloat(number);
+    console.log('previousValue: ' + previousValue);
+    console.log('currentValue: ' + currentValue)
+    if(isNaN(previousValue) || isNaN(currentValue)) 
+        return;
+    switch(operator){
+        case '+' : 
+        computation = previousValue + currentValue;
+        break;
+        case '-' : 
+        computation = previousValue - currentValue;
+        break;
+        case '*' : 
+        computation = previousValue * currentValue;
+        break;
+        case '/' : computation = previousValue / currentValue;
+        break;
+    }
+     
+    screen.innerHTML = computation;
+    previousNumber = computation;
+    number = '';
+}
